7f7b4f8f0a5119858a00e983d3b87778e0e7f1a8
omprakashch3 <omprakashchautala3@gmail.com>
2020-08-10 15:38:11 +0530

    running parralel promises

--- promise-api.js
+++ promise-api.js
@@ -2,5 +2,30 @@
 // p.then((result) => console.log(result));
 
 // const q = Promise.reject(new Error("error for rejection...."));
-const q = Promise.reject("Reason for rejection");
-q.catch((error) => console.log(error));
+// // const q = Promise.reject("Reason for rejection");
+// q.catch((error) => console.log(error));
+
+// running parrallel promises
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("RUNNING ASYNCHRONOUS 1...");
+    // reject(new Error("something went wrong"));
+    resolve(1);
+  }, 3000);
+});
+
+const p2 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("RUNNING ASYNCHRONOUS 2...");
+    resolve(3);
+  }, 4000);
+});
+
+// Promise.all([p1, p2])
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log("Error", err.message));
+
+//   as soon as one promise fullfilled return it
+Promise.race([p1, p2])
+  .then((result) => console.log(result))
+  .catch((err) => console.log("Error", err.message));
