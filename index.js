a5edf5e1b1226c31a2caf8bfb89bf5cc5f477038
omprakashch3 <omprakashchautala3@gmail.com>
2020-08-10 15:57:24 +0530

    async await operation

--- index.js
+++ index.js
@@ -11,11 +11,26 @@ console.log("Before");
 // p.then((user) => console.log(user));
 // // alternate of above
 
-getUser(1)
-  .then((user) => getRepositories(user.gitHubUsername))
-  .then((repos) => getCommits(repos[0]))
-  .then((commits) => console.log("Commits", commits))
-  .catch((err) => console.log("Error", err.message));
+// promish based approch
+// getUser(1)
+//   .then((user) => getRepositories(user.gitHubUsername))
+//   .then((repos) => getCommits(repos[0]))
+//   .then((commits) => console.log("Commits", commits))
+//   .catch((err) => console.log("Error", err.message));
+
+// asynch await approch
+async function displayCommits() {
+  try {
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  } catch (err) {
+    console.log("Error", err.message);
+  }
+}
+
+displayCommits();
 
 console.log("After");
 
@@ -33,7 +48,8 @@ function getRepositories(username) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log("Calling GitHub API...");
-      resolve(["repo1", "repo2", "repo3"]);
+      // resolve(["repo1", "repo2", "repo3"]);
+      reject(new Error("coundnot get to the repos"));
     }, 2000);
   });
 }
